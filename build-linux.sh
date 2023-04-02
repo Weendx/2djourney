f6e9c9592b0e6780d588d34438c9df6d352853d4
Weendx <muha10bin@list.ru>
2023-04-02 15:54:42 +0300

    Move header files into the include dir

--- build-linux.sh
+++ build-linux.sh
@@ -3,5 +3,8 @@
 if ! [ $1 ]; then
   rm -rf build
 fi
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+if ! [ -e "compile_commands.json" ]; then
+  ln -s build/compile_commands.json compile_commands.json
+fi
 cmake --build build
